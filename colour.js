4cb1ac92593bb692008404fb8a76d5767a4fbe80
Meghavi02 <110556072+Meghavi02@users.noreply.github.com>
2023-01-13 18:40:48 +0530

    Add files via upload

---
+++ colour.js
@@ -0,0 +1,11 @@
+function zoomIn() {
+    var image = document.getElementById('myImage');
+    image.style.width = "150%";
+    image.style.height = "150%";
+}
+
+function zoomOut() {
+    var image = document.getElementById('myImage');
+    image.style.width = "100%";
+    image.style.height = "100%";
+}
